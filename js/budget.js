82cd796e88df505e69bcb234f6c85ddc8fc2c417
JNKarif <arif.rahman041@gmail.com>
2022-08-23 15:11:12 +0600

    functions-added

--- js/budget.js
+++ js/budget.js
@@ -1,55 +1,106 @@
+// document.getElementById('calc-btn').addEventListener('click', function () {
+//     const playerNumberElement = document.getElementById('selected-number');
+//     const playerNumberString = playerNumberElement.innerText;
+//     const playerNumber = parseFloat(playerNumberString);
+
+
+//     const playerInputField = document.getElementById('player-input');
+//     const playerInputFieldString = playerInputField.value;
+//     const perPlayerCost = parseFloat(playerInputFieldString);
+
+
+//     const playerCostTextElement = document.getElementById('player-cost');
+//     const playerCostTextElementString = playerCostTextElement.innerText;
+//     const playerCostTextTotal = parseFloat(playerCostTextElementString);
+
+//     const newPlayerCostTotal = perPlayerCost * playerNumber;
+
+//     // playerCostTextElement.innerText = newPlayerCostTotal;
+//     // console.log(typeof newPlayerCostTotal)
+//     setTextElementValueById('player-cost', newPlayerCostTotal);
+//     return newPlayerCostTotal;
+// })
+
+
+// document.getElementById('calc-btn-total').addEventListener('click', function () {
+//     // let newPlayerCostTotal;
+//     const managerInputField = document.getElementById('manager-cost');
+//     const managerInputFieldString = managerInputField.value;
+//     const managerCost = parseFloat(managerInputFieldString);
+
+
+//     const coachInputField = document.getElementById('coach-cost');
+//     const coachInputFieldString = coachInputField.value;
+//     const coachCost = parseFloat(coachInputFieldString);
+
+
+//     const totalTextInputField = document.getElementById('total-cost');
+//     const totalTextInputFieldString = totalTextInputField.innerText;
+//     const totalCost = parseFloat(totalTextInputFieldString);
+
+//     const newPlayerCostTotal1 = document.getElementById('player-cost');
+//     const newPlayerCostTotalString = newPlayerCostTotal1.innerText;
+//     const newPlayerCostTotal = parseFloat(newPlayerCostTotalString);
+
+
+//     const finalCost = managerCost + coachCost + newPlayerCostTotal;
+
+//     // totalTextInputField.innerText = finalCost;
+//     setTextElementValueById('total-cost', finalCost);
+
+
+// })
+
 document.getElementById('calc-btn').addEventListener('click', function () {
-    const playerNumberElement = document.getElementById('selected-number');
-    const playerNumberString = playerNumberElement.innerText;
-    const playerNumber = parseFloat(playerNumberString);
+
+    const playerNumber = getTextFieldById('selected-number')
 
 
-    const playerInputField = document.getElementById('player-input');
-    const playerInputFieldString = playerInputField.value;
-    const perPlayerCost = parseFloat(playerInputFieldString);
+
+    const perPlayerCost = getInputFieldValueById('player-input');
 
 
-    const playerCostTextElement = document.getElementById('player-cost');
-    const playerCostTextElementString = playerCostTextElement.innerText;
-    const playerCostTextTotal = parseFloat(playerCostTextElementString);
+    const playerCostTextTotal = getTextFieldById('player-cost')
+
+
+
 
     const newPlayerCostTotal = perPlayerCost * playerNumber;
 
-    // playerCostTextElement.innerText = newPlayerCostTotal;
-    // console.log(typeof newPlayerCostTotal)
+
     setTextElementValueById('player-cost', newPlayerCostTotal);
     return newPlayerCostTotal;
 })
 
 
 document.getElementById('calc-btn-total').addEventListener('click', function () {
-    // let newPlayerCostTotal;
-    const managerInputField = document.getElementById('manager-cost');
-    const managerInputFieldString = managerInputField.value;
-    const managerCost = parseFloat(managerInputFieldString);
 
 
-    const coachInputField = document.getElementById('coach-cost');
-    const coachInputFieldString = coachInputField.value;
-    const coachCost = parseFloat(coachInputFieldString);
+
+    const managerCost = getInputFieldValueById('manager-cost');
 
 
-    const totalTextInputField = document.getElementById('total-cost');
-    const totalTextInputFieldString = totalTextInputField.innerText;
-    const totalCost = parseFloat(totalTextInputFieldString);
 
-    const newPlayerCostTotal1 = document.getElementById('player-cost');
-    const newPlayerCostTotalString = newPlayerCostTotal1.innerText;
-    const newPlayerCostTotal = parseFloat(newPlayerCostTotalString);
+    const coachCost = getInputFieldValueById('coach-cost');
+
+
+
+    const totalCost = getTextFieldById('total-cost');
+
+
+    const newPlayerCostTotal = getTextFieldById('player-cost');
 
 
     const finalCost = managerCost + coachCost + newPlayerCostTotal;
 
-    // totalTextInputField.innerText = finalCost;
+
     setTextElementValueById('total-cost', finalCost);
 
 
 })
+
+
+
 
 
 
--- js/utility.js
+++ js/utility.js
@@ -1,3 +1,30 @@
+// function setTextElementValueById(elementId, NewValue) {
+//     const textElement = document.getElementById(elementId);
+//     textElement.innerText = NewValue;
+
+// };
+
+
+
+// function 1
+function getInputFieldValueById(inputFieldId) {
+    const playerInputField = document.getElementById(inputFieldId);
+    const playerInputFieldString = playerInputField.value;
+    const perPlayerCost = parseFloat(playerInputFieldString);
+    return perPlayerCost;
+}
+
+// function 2
+function getTextFieldById(textFieldId) {
+    const playerNumberElement = document.getElementById(textFieldId);
+    const playerNumberString = playerNumberElement.innerText;
+    const playerNumber = parseFloat(playerNumberString);
+    return playerNumber;
+}
+
+
+
+// function 3
 function setTextElementValueById(elementId, NewValue) {
     const textElement = document.getElementById(elementId);
     textElement.innerText = NewValue;
